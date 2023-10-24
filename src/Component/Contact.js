3ca287c83e66faf5c29baa90a958b8223ce6ae3f
Yashcodesmith <139487156+Yashcodesmith@users.noreply.github.com>
2023-10-24 23:23:06 +0530

    Add files via upload

---
+++ src/Component/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+    return (
+        <section className="contact">
+            <h2>Contact Me</h2>
+            <p>Email: yash.singh2021d@vitstudent.ac.in </p>
+        </section>
+    );
+}
+
+export default Contact;
